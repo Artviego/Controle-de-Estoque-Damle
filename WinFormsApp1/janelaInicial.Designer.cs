24731c6444bf7659e36a4e4441b2d5fe749a4562
Artviego <formaformosa@gmail.com>
2025-02-27 23:53:23 -0300

    commit visual janela inicial

--- WinFormsApp1/janelaInicial.Designer.cs
+++ WinFormsApp1/janelaInicial.Designer.cs
@@ -28,12 +28,132 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "janelaInicial";
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(janelaInicial));
+            btnPeca = new Button();
+            btnLista = new Button();
+            panel1 = new Panel();
+            contextMenuStrip1 = new ContextMenuStrip(components);
+            panel2 = new Panel();
+            pictureBox1 = new PictureBox();
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            tabPage2 = new TabPage();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            tabControl1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // btnPeca
+            // 
+            btnPeca.Location = new Point(25, 35);
+            btnPeca.Name = "btnPeca";
+            btnPeca.Size = new Size(114, 25);
+            btnPeca.TabIndex = 0;
+            btnPeca.Text = "Cadastrar Peça";
+            btnPeca.UseVisualStyleBackColor = true;
+            // 
+            // btnLista
+            // 
+            btnLista.Location = new Point(25, 101);
+            btnLista.Name = "btnLista";
+            btnLista.Size = new Size(114, 23);
+            btnLista.TabIndex = 1;
+            btnLista.Text = "Lista das Peças";
+            btnLista.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.IndianRed;
+            panel1.Controls.Add(btnLista);
+            panel1.Controls.Add(btnPeca);
+            panel1.Location = new Point(1, 85);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(170, 367);
+            panel1.TabIndex = 2;
+            // 
+            // contextMenuStrip1
+            // 
+            contextMenuStrip1.Name = "contextMenuStrip1";
+            contextMenuStrip1.Size = new Size(61, 4);
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.Maroon;
+            panel2.Controls.Add(pictureBox1);
+            panel2.Location = new Point(1, 0);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(802, 90);
+            panel2.TabIndex = 4;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(21, -8);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(118, 98);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Location = new Point(169, 63);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(631, 389);
+            tabControl1.TabIndex = 5;
+            // 
+            // tabPage1
+            // 
+            tabPage1.Location = new Point(4, 24);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(623, 361);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "Peças";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            tabPage2.Location = new Point(4, 24);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(624, 359);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "tabPage2";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // janelaInicial
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(tabControl1);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Name = "janelaInicial";
+            Text = "janelaInicial";
+            panel1.ResumeLayout(false);
+            panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            tabControl1.ResumeLayout(false);
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button btnPeca;
+        private Button btnLista;
+        private Panel panel1;
+        private ContextMenuStrip contextMenuStrip1;
+        private Panel panel2;
+        private PictureBox pictureBox1;
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
     }
 }
